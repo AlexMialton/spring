e9f150d58bd15a8144857a4c53cd6b75a2388ed6
aleksandramialton <smyalton@gmail.com>
2026-04-03 07:44:18 +0200

    dependency injection with owr own annotation

---
+++ src/main/java/spring/bpp/InjectBeanPostProcessor.java
@@ -0,0 +1,38 @@
+package spring.bpp;
+
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.util.ReflectionUtils;
+
+import java.util.Arrays;
+
+public class InjectBeanPostProcessor implements BeanPostProcessor, ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
+
+    @Override
+    public @Nullable Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Arrays.stream(bean.getClass().getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(InjectBean.class))
+                .forEach(field -> {
+                    var objectToInject = applicationContext.getBean(field.getType());
+                    ReflectionUtils.makeAccessible(field);
+                    ReflectionUtils.setField(field, bean, objectToInject);
+                    //or like below, but then you need to deal with Exception
+//                    field.setAccessible(true);
+//                    field.set(bean, objectToInject);
+
+                });
+
+        return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+    }
+
+    @Override
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
--- src/main/java/spring/database/repository/UserRepository.java
+++ src/main/java/spring/database/repository/UserRepository.java
@@ -2,33 +2,15 @@ package spring.database.repository;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-import lombok.Setter;
 import lombok.ToString;
+import spring.bpp.InjectBean;
+import spring.database.repository.pool.ConnectionPool;
 
-import java.util.List;
-import java.util.Map;
 
 @ToString
-@AllArgsConstructor
-@NoArgsConstructor(force = true) //to make bean creation with [setter] possible
-@Setter                          //to make bean creation with [setter] possible
 public class UserRepository {
-    private String userName;
-    private int poolSize;
-    private List<Object> args;
-    private Map<String, Object> properties;
+    //@InjectBean // the same as @Autowire
+    private ConnectionPool connectionPool;
 
-// Needed for Bean Definition creation
 
-    @PostConstruct // Needed for Bean Definition creation with [annotation]
-    public void init(){  // Needed for Bean Definition creation with [xml]
-        System.out.println("Init UserRepository");
-    }
-    @PreDestroy
-    public void destroy(){
-        System.out.println("Destroy UserRepository");
-    }
 }
